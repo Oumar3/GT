43a3cdbc7413b607f9430fcd839008383bb5ff1f
OUMAR3 <oumaralitori1@gmail.com>
2023-08-05 00:32:52 +0100

    update settings

--- GroupTass/settings.py
+++ GroupTass/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'GroupTassApp',
 ]
 
@@ -83,6 +86,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#Boket s3
+
+AWS_ACCESS_KEY_ID = 'AKIAXTSUZKLCO46AAND4'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'grouptass'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
 # Password validation
